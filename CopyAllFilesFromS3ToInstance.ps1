2213742813c022b95347a231ec2e5c7ebb2a5d17
Danny Davis <pko3@gmx.de>
2015-06-04 13:14:03 +0200

    created script to copy files
    script should copy all files from a folder in a s3bucket to a local
    path, usually an instance.

---
+++ CopyAllFilesFromS3ToInstance.ps1
@@ -0,0 +1,28 @@
+#get-module -ListAvailable
+
+import-module AWSPowerShell
+
+#aws
+$s3bucket = "Bucketname"
+$region = "eu-central-1"
+$access = "AccessKey"
+$secret = "SecretKey"
+$folder = "FolderNameInS3Bucket"
+
+#local machine
+$path = "LocalPath"
+
+write-host "Getting the bucket: " $s3bucket
+$files = Get-S3Object -BucketName $s3bucket -KeyPrefix $folder -AccessKey $access -SecretKey $secret #-Region $region
+
+write-host "crawling through files now..."
+foreach($file in $files)
+{
+    if($file.key -ne "")
+    {
+        write-host "File: " $file.Key
+        Copy-S3Object -BucketName $s3bucket -Key $file.Key -LocalFile -AccessKey $access -SecretKey $secret #-Region $region
+    }
+}
+
+Read-Host "Press ENTER to exist..."
